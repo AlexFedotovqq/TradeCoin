a013f1a2424d7b462c4aa576b513ec2332bbf30f
pfed-prog <fedorovn19@gmail.com>
2022-11-28 17:05:49 +0100

    build errors fixed

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# FEVM
-
-## Resources
-
-https://github.com/filecoin-project/FEVM-Hardhat-Kit
-
-[Lending Pool Cookbook](https://fvm-forum.filecoin.io/t/lending-pool-cookbook/114)
+# XRC-Swap
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,6 +1,7 @@
-import { Popover, Transition } from '@headlessui/react'
+import { Popover } from "@headlessui/react";
+import Link from "next/link";
 
-export default function Example() {
+export default function Index() {
   return (
     <div className="relative overflow-hidden bg-orange-400 h-screen">
       <div className="mx-auto max-w-7xl">
@@ -17,38 +18,36 @@ export default function Example() {
 
           <Popover>
             <div className="relative px-4 pt-6 sm:px-6 lg:px-8">
-              <nav className="relative flex items-center justify-between sm:h-12 lg:justify-start" aria-label="Global">
-   
-              </nav>
+              <nav
+                className="relative flex items-center justify-between sm:h-12 lg:justify-start"
+                aria-label="Global"
+              ></nav>
             </div>
-
           </Popover>
 
           <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">XRC-Swap</span>{' '}
-                
+                <span className="block xl:inline">XRC-Swap</span>{" "}
               </h1>
               <p className="mt-3 text-base text-black sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
-              This is a multifunctional project for the exchange of cryptocurrencies
+                This is a multifunctional project for the exchange of
+                cryptocurrencies
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
-                  <a
-                    href="/home"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
-                  >
-                    Home
-                  </a>
+                  <Link href="/home">
+                    <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">
+                      Home
+                    </a>
+                  </Link>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="/exchange"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-700 hover:bg-gray-200 md:py-4 md:px-10 md:text-lg"
-                  >
-                    Exchange
-                  </a>
+                  <Link href="/exchange">
+                    <a className="flex w-full items-center justify-center rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-indigo-700 hover:bg-gray-200 md:py-4 md:px-10 md:text-lg">
+                      Exchange
+                    </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -63,5 +62,5 @@ export default function Example() {
         />
       </div>
     </div>
-  )
+  );
 }
