3279239052fc28c624e734d736c22648e4c831cb
AlexFedotovqq <alexkekfedotov@gmail.com>
2022-12-13 17:29:29 +0100

    added button

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -73,6 +73,14 @@ const Navbar = () => {
                     </div>
                   </div>
                 </div>
+                <div className="flex items-center md:ml-12">
+            <a
+              href="#"
+              className="ml-8 inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+            >
+              Connect wallet
+            </a>
+          </div>
               </div>
             </div>
             <Disclosure.Panel className="md:hidden">
