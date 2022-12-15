0dce6c867ee2413e95548c51e6b91dcd9cd45582
AlexFedotovqq <alexkekfedotov@gmail.com>
2022-12-15 19:12:07 +0100

    added

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -36,9 +36,9 @@ export default function Index() {
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
-                  <Link href="/home" className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">
+                  <Link href="/pool" className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">
                     
-                      Home
+                      Pool
                     
                   </Link>
                 </div>
