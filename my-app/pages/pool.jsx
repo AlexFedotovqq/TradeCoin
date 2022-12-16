f2fe648da7b649cce8e477baa5f0f85d5bf5b2ea
pfed-prog <fedorovn19@gmail.com>
2022-12-16 18:51:48 +0100

    centered div

--- my-app/pages/pool.jsx
+++ my-app/pages/pool.jsx
@@ -156,7 +156,7 @@ export default function Example() {
           </h2>
         </div>
         <div className="bg-orange-400">
-          <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-3 lg:px-1">
+          <div className="mx-auto flex justify-center py-12 px-4 sm:px-6">
             <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
               <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
                 <div className="inline-flex rounded-md shadow">
@@ -168,7 +168,7 @@ export default function Example() {
                             <Disclosure.Button className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white hover:bg-indigo-700">
                               <span
                                 className={classNames(
-                                  open ? "text-white" : "text-indigo-200",
+                                  open ? "text-indigo-200" : "text-white",
                                   "text-sm font-medium"
                                 )}
                               >
