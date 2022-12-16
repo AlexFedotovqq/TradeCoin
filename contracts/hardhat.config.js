5cb5ef8018ab117d289a9d30a754f99670c515e4
pfed-prog <fedorovn19@gmail.com>
2022-12-16 02:39:50 +0100

    added more deploys

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.5.16",
+
+  networks: {
+    apothem: {
+      url: "https://rpc.apothem.network/",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 51,
+    },
+    evmos: {
+      url: "https://eth.bd.evmos.dev:8545",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 9000,
+    },
+    xinfin: {
+      url: "https://erpc.xinfin.network",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
