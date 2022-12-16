2a51a1792a00b99c97c0c3a3a950250d724d43e9
pfed-prog <fedorovn19@gmail.com>
2022-12-16 19:03:57 +0100

    changing the form

--- my-app/pages/pool.jsx
+++ my-app/pages/pool.jsx
@@ -270,7 +270,7 @@ export default function Example() {
           >
             {people.map((person) => (
               <li
-                key={person.email}
+                key={person.name}
                 className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
               >
                 <div className="flex w-full items-center justify-between space-x-6 p-6">
@@ -279,35 +279,18 @@ export default function Example() {
                       <h3 className="truncate text-sm font-medium text-gray-900">
                         {person.name}
                       </h3>
-                      <span className="inline-block flex-shrink-0 rounded-full bg-green-100 px-2 py-0.5 text-xs font-medium text-green-800">
-                        {person.role}
-                      </span>
                     </div>
-                    <p className="mt-2 truncate text-sm text-gray-500">
-                      {person.title}
-                    </p>
                   </div>
-                  <img
-                    className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300"
-                    src={person.imageUrl}
-                    alt=""
-                  />
                 </div>
                 <div>
                   <div className="-mt-px flex divide-x divide-gray-200">
                     <div className="flex w-0 flex-1">
-                      <a
-                        href={`mailto:${person.email}`}
-                        className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                      >
+                      <a className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
                         <span className="ml-3">Add</span>
                       </a>
                     </div>
                     <div className="-ml-px flex w-0 flex-1">
-                      <a
-                        href={`tel:${person.telephone}`}
-                        className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500"
-                      >
+                      <a className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-lg border border-transparent py-4 text-sm font-medium text-gray-700 hover:text-gray-500">
                         <span className="ml-3">Remove</span>
                       </a>
                     </div>
