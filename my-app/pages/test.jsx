15f7b0765ff548f861585dca6ddd5ac7a4714429
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-01-13 20:26:21 +0100

    added

--- my-app/pages/test.jsx
+++ my-app/pages/test.jsx
@@ -8,92 +8,15 @@ export default function Example() {
 
   return (
     <div className="isolate bg-white">
-      <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-       
-       
-          
-          <defs>
-            <linearGradient
-              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-              x1="1155.49"
-              x2="-78.208"
-              y1=".177"
-              y2="474.645"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        
-      </div>
-      <div className="px-6 pt-6 lg:px-8">
-        <div>
-          <nav className="flex h-9 items-center justify-between" aria-label="Global">
-            
-            
-            
-            
-          </nav>
-          <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-            <Dialog.Panel focus="true" className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
-              <div className="flex h-9 items-center justify-between">
-                <div className="flex">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Your Company</span>
-                    <img
-                      className="h-8"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt=""
-                    />
-                  </a>
-                </div>
-                <div className="flex">
-                  <button
-                    type="button"
-                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                    onClick={() => setMobileMenuOpen(false)}
-                  >
-                    <span className="sr-only">Close menu</span>
-                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-                </div>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    
-                  </div>
-                  <div className="py-6">
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-                    >
-                      Log in
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Dialog.Panel>
-          </Dialog>
-        </div>
-      </div>
+
+      
       <main>
         <div className="relative px-6 lg:px-8">
-          <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+          <div className="mx-auto max-w-3xl pt-20 pb-32  sm:pb-40">
             <div>
-              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  <span className="text-gray-600">
-                    Announcing our next round of funding.{' '}
-                    <a href="#" className="font-semibold text-indigo-600">
-                      <span className="absolute inset-0" aria-hidden="true" />
-                      Read more <span aria-hidden="true">&rarr;</span>
-                    </a>
-                  </span>
-                </div>
-              </div>
+              
               <div>
+                
                 <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">
                 XRC-Swap
                 </h1>
@@ -102,10 +25,10 @@ export default function Example() {
                 </p>
                 <div className="mt-8 flex gap-x-4 sm:justify-center">
                   <a
-                    href="#"
+                    href="/exchange"
                     className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
                   >
-                    Get started{' '}
+                    Exchange{' '}
                     <span className="text-indigo-200" aria-hidden="true">
                       &rarr;
                     </span>
@@ -114,12 +37,134 @@ export default function Example() {
                     href="#"
                     className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
                   >
-                    Live demo{' '}
+                    Live demo video{' '}
                     <span className="text-gray-400" aria-hidden="true">
                       &rarr;
                     </span>
                   </a>
                 </div>
+              </div>
+              
+              <div>
+                
+              <div className="mt-8">
+                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-5xl">
+                About "Blog"
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                We have a "Blog" tab on our site.
+                </p>
+                <p className="mt-3 text-lg leading-3 text-gray-600 sm:text-center">
+                On this tab you can read something interesting from the field of crypto and so on.
+                </p>
+                <div className="mt-3"></div>
+                <div className="relative overflow-hidden  flex items-center justify-center">
+      <iframe src="/tenor (4).gif" width="580" height="445"  className='justify-center flex items-center' allowFullScreen></iframe>
+    </div>
+                <div className="mt-8 flex gap-x-4 sm:justify-center">
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                  <span className="text-gray-600">
+                  Here are the most interesting posts{' '}
+                    <a href="/blogs" className="font-semibold text-indigo-600">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      View our blog <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </span>
+                </div>
+                </div>
+              </div>
+              </div>
+              </div>
+              <div>
+              <div className="mt-8">
+                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-5xl">
+                About "Exchange"
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                We have a "Exchange" tab on our site.
+                </p>
+                <p className="mt-3 text-lg leading-3 text-gray-600 sm:text-center">
+                On this tab you can exchange cryptocurrency
+                </p>
+                <div className="mt-3"></div>
+                <div className="relative overflow-hidden  flex items-center justify-center">
+      <iframe src="/tenor.gif" width="480" height="459"  className='justify-center flex items-center' allowFullScreen></iframe>
+    </div>
+                <div className="mt-3 flex gap-x-4 sm:justify-center">
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                  <span className="text-gray-600">
+                  Here are the most interesting exchange{' '}
+                    <a href="/exchange" className="font-semibold text-indigo-600">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      View our Exchange <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </span>
+                </div>
+                </div>
+              </div>
+              </div>
+              </div>
+              <div>
+              <div className="mt-8">
+                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-5xl">
+                About "Pool"
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                We have a "Pool" tab on our site.
+                </p>
+                <p className="mt-3 text-lg leading-3 text-gray-600 sm:text-center">
+                -----------------------------something-----------------------------------------
+                </p>
+                <div className="mt-3"></div>
+                <div className="relative overflow-hidden  flex items-center justify-center">
+      <iframe src="/tenor (3).gif" width="580" height="390"  className='justify-center flex items-center' allowFullScreen></iframe>
+    </div>
+                <div className="mt-8 flex gap-x-4 sm:justify-center">
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                  <span className="text-gray-600">
+                  Here are the most interesting pool{' '}
+                    <a href="/pool" className="font-semibold text-indigo-600">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      View our pool <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </span>
+                </div>
+                </div>
+              </div>
+              </div>
+              </div>
+              <div>
+              <div className="mt-8">
+                <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-5xl">
+                About "Contact us"
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
+                We have a "Contact us" tab on our site.
+                </p>
+                <p className="mt-3 text-lg leading-3 text-gray-600 sm:text-center">
+                -----------------------------something-----------------------------------------
+                </p>
+                <div className="mt-3"></div>
+                <div className="relative overflow-hidden  flex items-center justify-center">
+      <iframe src="/tenor (2).gif" width="510" height="400"  className='justify-center flex items-center' allowFullScreen></iframe>
+    </div>
+                <div className="mt-8 flex gap-x-4 sm:justify-center">
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                  <span className="text-gray-600">
+                  Here are the most interesting pool{' '}
+                    <a href="/pool" className="font-semibold text-indigo-600">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      Contact us <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </span>
+                </div>
+                </div>
+              </div>
+              </div>
               </div>
               <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg
