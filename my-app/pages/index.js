8db3984252f11718e2047030d9a4538e3638e369
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-01-29 01:14:05 +0100

    added

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -8,21 +8,11 @@ const myLoader = ({ src, width }) => {
 export default function Index() {
   return (
     <div>
-
-      
-      <div className="relative overflow-hidden bg-white ">
+      <div className="relative overflow-hidden bg-gradient-to-r from-violet-300 to-fuchsia-800">
         <div className="mx-auto max-w-7xl">
-          <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+          <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
             
-            <svg
-              className="absolute inset-y-0 right-0 hidden h-screen w-48 translate-x-1/2 transform text-white lg:block"
-              fill="currentColor"
-              viewBox="0 0 100 100"
-              preserveAspectRatio="none"
-              aria-hidden="true"
-            >
-              <polygon points="50,0 100,0 50,100 0,100" />
-            </svg>
+         
 
             
 
