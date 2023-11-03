51c98bd85d0df5c59b8a9cd724d577718b392e7e
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-11-03 20:29:56 +0100

    update Navbar

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -39,13 +39,13 @@ const Navbar = () => {
   }
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-gray-800 ">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
+            <div className="flex h-16 justify-between ">
               <div className="flex">
-                <div className="-ml-2 mr-2 flex items-center md:hidden">
+                <div className="-ml-2 mr-2 flex items-center md:hidden ">
                   {/* Mobile menu button */}
                   <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
@@ -57,22 +57,36 @@ const Navbar = () => {
                   </Disclosure.Button>
                 </div>
 
-                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "rounded-md px-3 py-2 text-md font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
+                <div className="hidden  md:flex md:items-center md:space-x-4 mt-3.5">
+                  <ul
+                    className="menu"
+                    style={{
+                      display: "flex",
+                      color: "#FFFFFF",
+                    }}
+                  >
+                    {navigation.map((item) => (
+                      <li
+                        key={item.name}
+                        style={{
+                          width: "125px",
+                          height: "50px",
+                          textAlign: "center",
+                        }}
+                      >
+                        <a
+                          href={item.href}
+                          style={{
+                            fontSize: "20px",
+
+                            transition: "all 0.45s",
+                          }}
+                        >
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
               </div>
 
