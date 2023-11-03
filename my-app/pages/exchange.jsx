513ea4f6bb67ef07c37de931eb6cc09140a63120
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-11-03 14:51:37 +0100

    add

--- my-app/pages/exchange.jsx
+++ my-app/pages/exchange.jsx
@@ -1,6 +1,8 @@
 import { getContractInfo, getERC20, getPair } from "@/utils/contracts";
 import { Dialog, Transition } from "@headlessui/react";
 import { Combobox } from "@headlessui/react";
+import { tokens } from "@/utils/tokens";
+import { classNames } from "@/utils/classNames";
 import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import {
   CheckCircleIcon,
@@ -10,41 +12,9 @@ import {
 import { CheckIcon } from "@heroicons/react/24/outline";
 import { Fragment, useState } from "react";
 
-function expandTo18Decimals(n) {
+export function expandTo18Decimals(n) {
   return ethers.BigNumber.from(n).mul(ethers.BigNumber.from(10).pow(18));
 }
-
-const tokens = [
-  {
-    id: 1,
-    name: "TradeC0",
-    address: "41377a640a0bf48d4c5ab79f63d2e4885659b82a29",
-    imageUrl: "/TradeC0.jpg",
-  },
-  {
-    id: 2,
-    name: "TradeC1",
-    address: "4191447b0204cf766eaf5f3f44d31370c870ec3f45",
-    imageUrl: "/TradeC1.jpg",
-  },
-  {
-    id: 3,
-    name: "Dspyt",
-    address: "412baca645bf7d8249eee9fd1b67dd2457dc76cdd6",
-    imageUrl: "/Dspyt.png",
-  },
-  {
-    id: 4,
-    name: "TradeCoin",
-    address: "413e152ac3ebbb60fd4af26fcfa0938189383a38f1",
-    imageUrl: "/TradeCoin.png",
-  },
-];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Exchange() {
   const [isLoading, setIsLoading] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
---
+++ my-app/utils/tokens.js
@@ -0,0 +1,26 @@
+export const tokens = [
+  {
+    id: 1,
+    name: "TradeC0",
+    address: "41377a640a0bf48d4c5ab79f63d2e4885659b82a29",
+    imageUrl: "/TradeC0.jpg",
+  },
+  {
+    id: 2,
+    name: "TradeC1",
+    address: "4191447b0204cf766eaf5f3f44d31370c870ec3f45",
+    imageUrl: "/TradeC1.jpg",
+  },
+  {
+    id: 3,
+    name: "Dspyt",
+    address: "412baca645bf7d8249eee9fd1b67dd2457dc76cdd6",
+    imageUrl: "/Dspyt.png",
+  },
+  {
+    id: 4,
+    name: "TradeCoin",
+    address: "413e152ac3ebbb60fd4af26fcfa0938189383a38f1",
+    imageUrl: "/TradeCoin.png",
+  },
+];
