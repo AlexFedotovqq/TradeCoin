5bec2b001038ad1a1575f32856af4cfcd062a24b
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-11-04 01:40:35 +0100

    update home page

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -14,7 +14,7 @@ const navigation = [
 
 const Navbar = () => {
   return (
-    <Disclosure as="nav" className="bg-gray-800 ">
+    <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
