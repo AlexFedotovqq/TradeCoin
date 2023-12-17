d674362b3502ddaae387bb64840c6f42cf69a670
AlexFedotovqq <alexkekfedotov@gmail.com>
2023-12-17 01:03:25 +0100

    fixed blogs

--- my-app/pages/blogs.jsx
+++ my-app/pages/blogs.jsx
@@ -9,8 +9,7 @@ const posts = [
       "The rise of decentralized finance (DeFi) has revolutionized the financial world, allowing anyone to access a variety of financial services and investments without relying on a centralized entity.",
     date: "Jan 12, 2022",
     datetime: "2022-01-12",
-    imageUrl:
-      "https://dspyt.com/_next/image?url=%2Fimages%2Fposts%2Fxrcswap%2Fimage.webp&w=640&q=75",
+    imageUrl: "/ds.webp",
     readingTime: "6 min",
     author: {
       name: "Our Twitter",
@@ -25,8 +24,7 @@ const posts = [
     description: "Download MetaMask wallet from the App Store or Google Play.",
     date: "Jan 16, 2022",
     datetime: "2022-01-16",
-    imageUrl:
-      "https://www.xdc.dev/images/sforPIcEaZSQqArN0fsKq1AXsnkGlnTkKa0xT-bsyeQ/s:1000:420/mb:500000/ar:1/aHR0cHM6Ly93d3cu/eGRjLmRldi91cGxv/YWRzL2FydGljbGVz/L3ZlNmUzZDl0aW56/ZTdma2p5aWZlLnBu/Zw",
+    imageUrl: "/bg2.jpg",
     readingTime: "3 min",
     author: {
       name: "Our Twitter",
@@ -42,8 +40,7 @@ const posts = [
       "On the XDC blockchain, cryptocurrencies (XRC-20 tokens) can be exchanged using the peer-to-peer XRC-Swap. In order to promote censorship resistance, security, and self-custody, the protocol is built as a collection of persistent, non-upgradable smart contracts. ",
     date: "Jan 17, 2022",
     datetime: "2022-01-17",
-    imageUrl:
-      "https://www.xdc.dev/images/IBOJm8etmlJM19eT93yZoQ4lMziudzwqXPdJ2_QLTAc/s:1000:420/mb:500000/ar:1/aHR0cHM6Ly93d3cu/eGRjLmRldi91cGxv/YWRzL2FydGljbGVz/L3ZiaTBjYmV6ZW4y/cmpwcGwzZWh5Lmpw/Zw",
+    imageUrl: "/dv.webp",
     readingTime: "6 min",
     author: {
       name: "Our Twitter",
