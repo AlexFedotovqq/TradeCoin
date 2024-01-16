a4f6850b69298a139a216fd0a9b60f46be28d85a
AlexFedotovqq <alexkekfedotov@gmail.com>
2024-01-16 02:19:07 +0100

    Added new Application instruction

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -196,24 +196,27 @@ export default function Index() {
                         use the application?
                       </span>
                     </p>
-                    <p className="mt-4 text-lg leading-8 text-gray-900 sm:text-center">
-                      <strong>
-                        Then you can watch the{" "}
-                        <Link
-                          href="https://www.youtube.com/embed/BUigc1HjJEw?autoplay=1&mute=1"
-                          className="font-extrabold  text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 sm:text-center"
-                        >
-                          YouTube video
-                        </Link>
-                        . It clearly shows you how to use it.
-                      </strong>
-                    </p>
                     <p className="mt-4 mb-9 text-lg leading-8 text-gray-900 sm:text-center">
-                      <strong>
-                        How to use TradeCoin: Unlock liquidity with TradeCoin -
-                        empowering decentralized finance
-                      </strong>
+                      <strong>Then you can visit and study our </strong>
+                      <span className="font-extrabold  text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 sm:text-center">
+                        {" "}
+                        documentation!
+                      </span>
                     </p>
+                    <div className="mt-3 flex items-center gap-x-4 justify-center">
+                      <div className=" sm:mb-8 sm:flex sm:justify-center">
+                        <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 bg-gradient-to-br from-pink-400 to-red-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                          <span className="text-gray-900">
+                            <Link
+                              href="https://alexfedotovqq.github.io/DocsTradeCoin/#/./howdoesitwork/how-does-it-work"
+                              className="font-semibold text-white"
+                            >
+                              View our Docs
+                            </Link>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
 
