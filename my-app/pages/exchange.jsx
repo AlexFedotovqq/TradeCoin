81f7ad5f0d6550783d0f0a81d9ec6b8ca95e81bb
AlexFedotovqq <alexkekfedotov@gmail.com>
2024-02-25 17:30:32 +0100

    The function (WarningTransition) has been moved to another file

--- my-app/pages/exchange.jsx
+++ my-app/pages/exchange.jsx
@@ -2,6 +2,7 @@ import SuccessMessage from "../components/Exchange/SuccessMessage";
 import ExchangeButton from "../components/Exchange/ExchangeButton";
 import TokenASelector from "../components/Exchange/TokenASelector";
 import TokenBSelector from "../components/Exchange/TokenBSelector";
+import WarningTransition from "../components/Exchange/WarningTransition";
 
 import { tokens } from "@/utils/tokens";
 import { classNames } from "@/utils/classNames";
@@ -23,7 +24,7 @@ export default function Exchange() {
 
   const [query, setQuery] = useState("");
 
-  // warning
+  // warning enable via true
   const [show, setShow] = useState(false);
 
   const [openTokenA, setOpenTokenA] = useState(false);
@@ -67,51 +68,7 @@ export default function Exchange() {
           className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
         >
           <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
-            <Transition
-              show={show}
-              as={Fragment}
-              enter="transform ease-out duration-300 transition"
-              enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-              enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-yellow-50 shadow-lg ring-1 ring-black ring-opacity-5">
-                <div className="p-4">
-                  <div className="flex items-start">
-                    <div className="flex-shrink-0">
-                      <ExclamationTriangleIcon
-                        className="h-6 w-6 text-yellow-400"
-                        aria-hidden="true"
-                      />
-                    </div>
-
-                    <div className="ml-3 w-0 flex-1 pt-0.5">
-                      <p className="text-sm font-medium text-gray-900">
-                        Warning!
-                      </p>
-                      <p className="mt-1 text-sm text-gray-500">
-                        Don&apos;t forget to connect your wallet
-                      </p>
-                    </div>
-
-                    <div className="ml-4 flex flex-shrink-0">
-                      <button
-                        type="button"
-                        className="inline-flex rounded-md bg-yellow-50 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        onClick={() => {
-                          setShow(false);
-                        }}
-                      >
-                        <span className="sr-only">Close</span>
-                        <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Transition>
+            <WarningTransition show={show} setShow={setShow} />
             <SuccessMessage isSuccess={isSuccess} />
           </div>
         </div>
