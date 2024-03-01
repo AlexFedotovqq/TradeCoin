655eca0f0527f370a45389b7093df7fd156ae970
alex <alexkekfedotov@gmail.com>
2024-03-01 18:19:13 +0100

    added 2 scripts for dex

---
+++ mina/src/scripts/deployDex.ts
@@ -0,0 +1,20 @@
+import { PrivateKey } from "o1js";
+import dotenv from "dotenv";
+
+import { startBerkeleyClient } from "../helpers/client.js";
+import { deployDex } from "../dex/dex.js";
+
+dotenv.config();
+
+const compile = true;
+const live = true;
+
+startBerkeleyClient();
+
+const zkDexAppPrivateKey = PrivateKey.random();
+
+const pk = PrivateKey.fromBase58(process.env.pk ?? "");
+
+await deployDex(zkDexAppPrivateKey, pk, compile, live);
+
+console.log(zkDexAppPrivateKey.toBase58());
---
+++ mina/src/scripts/queryDex.ts
@@ -0,0 +1,19 @@
+import { PrivateKey, fetchAccount } from "o1js";
+import dotenv from "dotenv";
+
+import { startBerkeleyClient } from "../helpers/client.js";
+import { logDexStates } from "../helpers/logs.js";
+import { Dex } from "../DexContract.js";
+
+dotenv.config();
+
+startBerkeleyClient();
+
+const zkDexAppPrivateKey = PrivateKey.fromBase58(process.env.zkAppPK ?? "");
+const pub = zkDexAppPrivateKey.toPublicKey();
+
+const dex = new Dex(pub);
+
+await fetchAccount({ publicKey: pub });
+
+logDexStates(dex);
