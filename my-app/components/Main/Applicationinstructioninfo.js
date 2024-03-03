70069e77bb764a1c6c63b83e128941f044e6cafa
alex <alexkekfedotov@gmail.com>
2024-03-03 20:50:28 +0100

    improving homepage

--- my-app/components/Main/Applicationinstructioninfo.js
+++ my-app/components/Main/Applicationinstructioninfo.js
@@ -4,17 +4,17 @@ export function ApplicationInstructionInfo() {
   return (
     <div className="mt-8">
       <h1 className="text-3xl font-bold tracking-tight sm:text-center sm:text-5xl">
-        <strong>Application instruction</strong>
+        Application instruction
       </h1>
       <p className="mt-6 text-lg leading-8 text-black sm:text-center">
-        <strong>Faced with the fact that you do not know how to</strong>
+        Faced with the fact that you do not know how to
         <span className="font-extrabold  text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 sm:text-center">
           {" "}
           use the application?
         </span>
       </p>
       <p className="mt-4 mb-9 text-lg leading-8 text-gray-900 sm:text-center">
-        <strong>Then you can visit and study our </strong>
+        Then you can visit and study our
         <span className="font-extrabold  text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600 sm:text-center">
           {" "}
           documentation!
@@ -23,10 +23,10 @@ export function ApplicationInstructionInfo() {
       <div className="mt-3 flex items-center gap-x-4 justify-center">
         <div className=" sm:mb-8 sm:flex sm:justify-center">
           <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 bg-gradient-to-br from-pink-400 to-red-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            <span className="text-gray-900">
+            <span className="">
               <Link
                 href="https://alexfedotovqq.github.io/DocsTradeCoin/#/"
-                className="font-semibold text-white"
+                className="font-semibold text-gray-900 text-white"
               >
                 View our Docs
               </Link>
